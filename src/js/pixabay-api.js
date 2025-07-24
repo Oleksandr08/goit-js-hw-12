07417b8f98727f93270849ff34235e032b022c64
Oleksandr08 <is4uk2011@icloud.com>
2025-07-24 18:00:08 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,16 +1,41 @@
 import axios from 'axios';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '51418093-89174c8449e8a26154b177f03'; 
-export async function getImagesByQuery(query) {
-  const params = {
-    key: API_KEY,
-    q: query,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-  };
 
-  const response = await axios.get(BASE_URL, { params });
-  return response.data;
+export async function fetchImages(query, currentPage) {
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 40,
+        page: currentPage,
+      },
+    });
+
+    if (response.data.hits.length === 0) {
+      iziToast.error({
+        message: 'Sorry, there are no images matching your search query.',
+        position: 'topRight',
+      });
+    }
+
+    return {
+      result: response.data.hits,
+      totalHits: response.data.totalHits,
+    };
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      message: 'Something went wrong. Try again later',
+      position: 'topRight',
+    });
+    console.error('Error: ', error);
+  }
 }
