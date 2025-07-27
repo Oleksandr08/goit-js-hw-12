c821e8b047ed0cb8217cea5bbcd75b23176226a8
Oleksandr08 <is4uk2011@icloud.com>
2025-07-27 18:35:14 +0300

    Update render-functions.js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,36 +1,28 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import iziToast from 'izitoast';
 
 let lightbox;
 
 export function renderGallery(cards) {
-  return cards
+  const markup = cards
     .map(card => {
-      let {
-        largeImageURL,
-        webformatURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      } = card;
       return `
-      <a href="${largeImageURL}" class="photo-card">
-        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+      <a href="${card.largeImageURL}" class="photo-card">
+        <img src="${card.webformatURL}" alt="${card.tags}" loading="lazy" />
         <div class="info">
-          <p><strong>Likes:</strong> ${likes}</p>
-          <p><strong>Views:</strong> ${views}</p>
-          <p><strong>Comments:</strong> ${comments}</p>
-          <p><strong>Downloads:</strong> ${downloads}</p>
+          <p><strong>Likes:</strong> ${card.likes}</p>
+          <p><strong>Views:</strong> ${card.views}</p>
+          <p><strong>Comments:</strong> ${card.comments}</p>
+          <p><strong>Downloads:</strong> ${card.downloads}</p>
         </div>
       </a>
     `;
     })
     .join('');
-}
 
-export function initializeLightbox() {
+  document.querySelector('.gallery').insertAdjacentHTML('beforeend', markup);
+
   if (lightbox) {
     lightbox.refresh();
   } else {
@@ -41,5 +33,43 @@ export function initializeLightbox() {
       closeText: '×',
       navText: ['<', '>'],
     });
+  }
+}
+
+export function clearGallery() {
+  document.querySelector('.gallery').innerHTML = '';
+}
+
+export function showElement(el) {
+  el.classList.remove('hidden');
+}
+
+export function hideElement(el) {
+  el.classList.add('hidden');
+}
+
+export function scrollPage() {
+  const info = document.querySelector('.gallery a');
+  if (!info) return;
+  const height = info.getBoundingClientRect().height * 2;
+
+  window.scrollBy({
+    top: height,
+    behavior: 'smooth',
+  });
+}
+
+export function checkBtnLoadMoreStatus(params, loadMoreBtn) {
+  const perPage = 40;
+  const maxPages = Math.ceil(params.total / perPage);
+
+  if (params.page >= maxPages) {
+    hideElement(loadMoreBtn);
+    iziToast.info({
+      message: "We're sorry, but you've reached the end of search results.",
+      position: 'topRight',
+    });
+  } else {
+    showElement(loadMoreBtn);
   }
 }
