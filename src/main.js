463bff83acffee5821a8663837fc4e38ca47494f
Oleksandr08 <is4uk2011@icloud.com>
2025-07-27 18:35:16 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
 import { fetchImages } from './js/pixabay-api';
-import { renderGallery, initializeLightbox } from './js/render-functions';
+import {
+  renderGallery,
+  clearGallery,
+  showElement,
+  hideElement,
+  scrollPage,
+  checkBtnLoadMoreStatus,
+} from './js/render-functions';
 
 const form = document.querySelector('.form');
 const gallery = document.querySelector('.gallery');
@@ -18,7 +26,7 @@ const params = {
 form.addEventListener('submit', async event => {
   event.preventDefault();
 
-  params.query = event.target.elements.searchQuery.value;
+  params.query = event.target.elements.searchQuery.value.trim();
   params.page = 1;
 
   if (!params.query) {
@@ -29,7 +37,7 @@ form.addEventListener('submit', async event => {
     return;
   }
 
-  gallery.innerHTML = '';
+  clearGallery();
   showElement(loader);
   input.classList.add('active');
 
@@ -41,12 +49,10 @@ form.addEventListener('submit', async event => {
       return;
     }
 
-    const markup = renderGallery(result);
-    gallery.innerHTML = markup;
+    renderGallery(result);
     params.total = totalHits;
 
-    initializeLightbox();
-    checkBtnLoadMoreStatus();
+    checkBtnLoadMoreStatus(params, loadMore);
   } catch (error) {
     iziToast.error({
       title: 'Error',
@@ -55,22 +61,22 @@ form.addEventListener('submit', async event => {
     });
     console.error('Error', error);
   } finally {
+    form.searchQuery.value = '';
     hideElement(loader);
     input.classList.remove('active');
   }
 });
 
-loadMore.addEventListener('click', async event => {
+loadMore.addEventListener('click', async () => {
   showElement(loader);
   try {
     hideElement(loadMore);
     params.page += 1;
-    const { result } = await fetchImages(params.query, params.page);
-    const markup = renderGallery(result);
-    gallery.insertAdjacentHTML('beforeend', markup);
-    checkBtnLoadMoreStatus();
 
-    initializeLightbox();
+    const { result } = await fetchImages(params.query, params.page);
+    renderGallery(result);
+
+    checkBtnLoadMoreStatus(params, loadMore);
     scrollPage();
   } catch (error) {
     iziToast.error({
@@ -83,31 +89,3 @@ loadMore.addEventListener('click', async event => {
     hideElement(loader);
   }
 });
-
-function checkBtnLoadMoreStatus() {
-  const perPage = 40;
-  const maxPages = Math.ceil(params.total / perPage);
-  if (params.page >= maxPages) {
-    hideElement(loadMore);
-    iziToast.info({
-      message: "We're sorry, but you've reached the end of search results.",
-      position: 'topRight',
-    });
-  } else {
-    showElement(loadMore);
-  }
-}
-function showElement(e) {
-  e.classList.remove('hidden');
-}
-function hideElement(e) {
-  e.classList.add('hidden');
-}
-function scrollPage() {
-  const info = gallery.firstElementChild.getBoundingClientRect();
-  const height = info.height * 2;
-  window.scrollBy({
-    behavior: 'smooth',
-    top: height,
-  });
-}
